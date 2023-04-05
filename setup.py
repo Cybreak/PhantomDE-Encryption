c21fa4ddff5ac66e99a23b595b36e0a4e0ef1eb0
Cybreak <98296143+Cybreak@users.noreply.github.com>
2023-04-05 15:39:20 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+
+setuptools.setup(
+    name = "PhantomDE",
+    version = "0.1.2",
+    author = "Robert Jones",
+    author_email = "robertcwad@gmail.com",
+    description = ("Simple way to decode and encode strings!"),
+    keywords = "Python",
+    url = "http://packages.python.org/PhantomDE",
+    packages=setuptools.find_packages(),
+)
